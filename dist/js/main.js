80011063a80803cdad69f16bd755dc6e07645889
Roman Lukjanets <64837849+luteet@users.noreply.github.com>
2023-01-07 14:29:17 +0200

    💳

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,23 +1,7 @@
 
 const body = document.querySelector('body'),
     html = document.querySelector('html'),
-    menu = document.querySelectorAll('.header__burger, .header__nav, body'),
-    burger = document.querySelector('.header__burger'),
     header = document.querySelector('.header');
-
-
-body.addEventListener('click', function (event) {
-
-    function $(elem) {
-      return event.target.closest(elem)
-    }
-
-    // =-=-=-=-=-=-=-=-=-=- <Бургер в шапке> -=-=-=-=-=-=-=-=-=-=-
-
-    // =-=-=-=-=-=-=-=-=-=- </Бургер в шапке> -=-=-=-=-=-=-=-=-=-=-
-
-})
-
 
 // =-=-=-=-=-=-=-=-=-=-=-=- <slider> -=-=-=-=-=-=-=-=-=-=-=-=
 
@@ -25,41 +9,36 @@ let introSlider = new Swiper('.intro__slider', {
   
     spaceBetween: 30,
     slidesPerView: 2,
-
-		autoplay: {
-			delay: 0,
-			reverseDirection: true,
-			disableOnInteraction: false,
-			pauseOnMouseEnter: true,
-			//waitForTransition: false,
-		},
-		speed: 3000,
-		loop: true,
-		loopAdditionalSlides: 2,
-		freeMode: true,
+	autoplay: {
+		delay: 0,
+		reverseDirection: true,
+		disableOnInteraction: false,
+		pauseOnMouseEnter: true,
+	},
+	speed: 3000,
+	loop: true,
+	loopAdditionalSlides: 2,
+	freeMode: true,
     breakpoints: {
-      1185: {
-        slidesPerView: 6,
-      },
-			1015: {
-				spaceBetween: 86,
-			},
-			890: {
-				slidesPerView: 5,
-				spaceBetween: 60,
-			},
-			650: {
-				slidesPerView: 3,
-				spaceBetween: 60,
-			},
-			425: {
-				spaceBetween: 30,
-			}
-      /* 600: {
-        slidesPerView: 2,
-        centeredSlides: false,
-      }, */
+		1185: {
+			slidesPerView: 6,
+		},
+		1015: {
+			spaceBetween: 86,
+		},
+		890: {
+			slidesPerView: 5,
+			spaceBetween: 60,
+		},
+		650: {
+			slidesPerView: 3,
+			spaceBetween: 60,
+		},
+		425: {
+			spaceBetween: 30,
+		}
     }
+
 }); 
 
 // =-=-=-=-=-=-=-=-=-=-=-=- </slider> -=-=-=-=-=-=-=-=-=-=-=-=
